937314401b6446c0efb0eaa8a597351917c17d40
Yuke Zheng <yukezheng@Yukes-MacBook-Pro-2.local>
2020-09-08 13:58:27 -0400

    Updates

---
+++ precache-manifest.0ba715b96742f5922f441b2447ff9f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5512f6eded1f6a2480901a70b6aa5e",
+    "url": "/maps-of-venice/index.html"
+  },
+  {
+    "revision": "7c35168aea37e1fe4028",
+    "url": "/maps-of-venice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a5fde737027648b6891",
+    "url": "/maps-of-venice/static/js/2.3ae920cc.chunk.js"
+  },
+  {
+    "revision": "e19eaafa264efca74ec31ac90fea77b5",
+    "url": "/maps-of-venice/static/js/2.3ae920cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c35168aea37e1fe4028",
+    "url": "/maps-of-venice/static/js/main.3691063d.chunk.js"
+  },
+  {
+    "revision": "dd3d68179c1af949cbda",
+    "url": "/maps-of-venice/static/js/runtime-main.ab4854c1.js"
+  }
+]);
